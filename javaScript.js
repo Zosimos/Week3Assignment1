503fa851182b4ef33ad07d1d0efb1e1bf3aa557d
Zosimos <115514296+Zosimos@users.noreply.github.com>
2022-11-02 17:55:58 -0400

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,112 @@
+let age= 40;
+let country = "USA";
+let myName = "John Doe";
+let email = "john.doe@gmail.com";
+const tax = 11;
+let phone = "555-555-5555";
+let state = "Florida";
+let county = "Columbia";
+let color = "blue";
+let eyes = "hazel";
+let food = "steak";
+let drink = "pepsi";
+let jacket = "black";
+let hair = "brown";
+let dog = "frug";
+let cat = "tabby";
+let sky = "blue";
+let job = "programmer";
+let school = "SDGKU";
+let benefits = "VA";
+
+console.log(country, myName, age);
+console.log(phone, tax);
+console.log(email);
+console.log(food);
+
+
+document.write ("<p>" + myName +" lives in " + country+ "</p>");
+
+document.write ("<p> Age: " + age + "</p>");
+
+/*backtick (') indicates html - - - - Dollar symbol indicates JS*/
+
+document.write (`
+<p style = "color : blue"> name: ${myName} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> age: ${age} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> country: ${country} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> email: ${email} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> tax: ${tax} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> phone: ${phone} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> state: ${state} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> county: ${county} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> color: ${color} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> eyes: ${eyes} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> food: ${food} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> drink: ${drink} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> jacket: ${jacket} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> hair: ${hair} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> dog: ${dog} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> cat: ${cat} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> sky: ${sky} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> job: ${job} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> school: ${school} </p>
+`);
+
+document.write (`
+<p style = "color : blue"> benefits: ${benefits} </p>
+`);
